57ee2b9d5e0ba8d765e9be352cd28b0d39ae5edb
yonghee-cheon <yonghee.cheon@navercorp.com>
2021-01-25 20:47:54 +0900

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="direct_redis",
-    version="0.3.0",
+    version="0.3.1",
     license='MIT',
     author="Yonghee Cheon",
     author_email="yonghee.cheon@gmail.com",
